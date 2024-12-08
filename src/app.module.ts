5b7d1e7d2a86732b9739c7bc489f071007a77fe9
shodai <show.0417@icloud.com>
2024-12-09 05:37:04 +0900

    docker削除した

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaServiceProvider } from './prisma/prisma.provider';
+import { LectureModule } from './Lecture/lecture.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LectureModule],
+  providers: [AppService, PrismaServiceProvider],
+  exports: [PrismaServiceProvider],
 })
 export class AppModule {}
