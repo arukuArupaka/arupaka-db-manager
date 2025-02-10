3ff01a4f08049da4e5324678fdc607848375fd14
yuuki <bun.yuu88@gmail.com>
2025-02-10 18:23:07 +0900

    テーブルの修正

--- nestJS/prisma/migrations/20250210092139_create_listingitemtable/migration.sql
+++ nestJS/prisma/migrations/20250210092139_create_listingitemtable/migration.sql
@@ -4,14 +4,15 @@ CREATE TYPE "Condition" AS ENUM ('BRAND_NEW', 'LIKE_NEW', 'GOOD', 'FAIR', 'POOR'
 -- CreateTable
 CREATE TABLE "ListingItem" (
     "id" SERIAL NOT NULL,
+    "document_id" TEXT NOT NULL,
     "purchased_at" TIMESTAMP(3),
     "purchased_user_id" TEXT,
     "condition" "Condition" NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL,
     "department" "Academics" NOT NULL,
-    "description" TEXT NOT NULL,
+    "description" TEXT,
     "image_url" TEXT NOT NULL,
-    "price" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
 
     CONSTRAINT "ListingItem_pkey" PRIMARY KEY ("id")
