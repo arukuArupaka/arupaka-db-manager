bc62dce0632e7cbcc251c879ef0b819e97b2db00
yuuki <bun.yuu88@gmail.com>
2025-03-03 16:56:19 +0900

    未完成2

--- nestJS/src/listing-item/listing-item.service.ts
+++ nestJS/src/listing-item/listing-item.service.ts
@@ -18,18 +18,39 @@ export class SearchItemService {
       //全部select(imageurl含め)
       select:{id:true,documentId:true,purchasedAt:true,purchasedUserId:true,condition:true,createdAt:true,department:true,description:true,price:true,name:true,firebaseUserId:true,imageUrls:true}
     });
-    return items;
+    const newitems = items.map((item) => {
+      const newitem:ListingItemPayload = {
+        id: item.id,
+        createdAt: item.createdAt,
+        documentId: item.documentId,
+        condition: item.condition,
+        department: item.department,
+        imageUrls: item.imageUrls.map((imageUrl)=>{return imageUrl.url}),
+        price: item.price,
+        name: item.name,
+        firebaseUserId: item.firebaseUserId};
+      return newitem;
+    })
+    return newitems;
   }
 
   async ItemCreate(createItem: RecievedListingItemPayload): Promise<string> {
     validate(createItem).then((errors)=>{
       if(errors.length > 0){
         return errors;
-      }})
+      }});
+    const adjustedCreateItem = {
+      documentId: createItem.documentId,
+      condition: createItem.condition,
+      department: createItem.department,
+      price: createItem.price,
+      name: createItem.name,
+      firebaseUserId: createItem.firebaseUserId,
+    }  
     await this.prisma.listingItem.upsert({
       where: { documentId: createItem.documentId },
-      update: createItem,
-      create: createItem,
+      update: adjustedCreateItem,
+      create: adjustedCreateItem,
     });
     return 'OK';
   }
