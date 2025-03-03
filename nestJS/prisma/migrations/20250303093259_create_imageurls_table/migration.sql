4cd3e6bdc82297b6030cbe2893495b636bf56e36
yuuki <bun.yuu88@gmail.com>
2025-03-03 18:33:55 +0900

    マイグレーションの整理

--- nestJS/prisma/migrations/20250303093259_create_imageurls_table/migration.sql
+++ nestJS/prisma/migrations/20250303093259_create_imageurls_table/migration.sql
@@ -3,6 +3,8 @@
 
   - The values [Letters,SportandHealthScience,ScienceandEngineering,PharmaceuticalSciences,GastronomyManagement,BusinessAdministration,ComprehensivePsychology,ImageArtsandSciences,InformationScienceandEngineering] on the enum `Academics` will be removed. If these variants are still used in the database, this will fail.
   - You are about to drop the column `image_url` on the `listing_item` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[document_id]` on the table `listing_item` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `firebase_user_id` to the `listing_item` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterEnum
@@ -17,7 +19,9 @@ DROP TYPE "Academics_old";
 COMMIT;
 
 -- AlterTable
-ALTER TABLE "listing_item" DROP COLUMN "image_url";
+ALTER TABLE "listing_item" DROP COLUMN "image_url",
+ADD COLUMN     "firebase_user_id" TEXT NOT NULL,
+ALTER COLUMN "created_at" SET DEFAULT CURRENT_TIMESTAMP;
 
 -- CreateTable
 CREATE TABLE "image_url" (
@@ -27,6 +31,9 @@ CREATE TABLE "image_url" (
 
     CONSTRAINT "image_url_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "listing_item_document_id_key" ON "listing_item"("document_id");
 
 -- AddForeignKey
 ALTER TABLE "image_url" ADD CONSTRAINT "image_url_listing_item_id_fkey" FOREIGN KEY ("listing_item_id") REFERENCES "listing_item"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
