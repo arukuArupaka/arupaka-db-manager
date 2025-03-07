d84b91053653edee31bb8c31315526af0cecf3f6
show-creator <git>
2025-03-07 20:24:59 +0900

    教室と建物のテーブルを作成

---
+++ nestJS/prisma/migrations/20250307112416_create_building_and_classroom_table/migration.sql
@@ -0,0 +1,47 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `classRoom` on the `Building` table. All the data in the column will be lost.
+  - You are about to drop the column `period` on the `Building` table. All the data in the column will be lost.
+  - You are about to drop the column `school_year` on the `Building` table. All the data in the column will be lost.
+  - You are about to drop the column `semester` on the `Building` table. All the data in the column will be lost.
+  - You are about to drop the column `week_day` on the `Building` table. All the data in the column will be lost.
+  - You are about to drop the column `building` on the `Lecture` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Building" DROP COLUMN "classRoom",
+DROP COLUMN "period",
+DROP COLUMN "school_year",
+DROP COLUMN "semester",
+DROP COLUMN "week_day";
+
+-- AlterTable
+ALTER TABLE "Lecture" DROP COLUMN "building";
+
+-- CreateTable
+CREATE TABLE "ClassRoom" (
+    "id" SERIAL NOT NULL,
+    "building_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "ClassRoom_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LectureClassRoom" (
+    "id" SERIAL NOT NULL,
+    "lecture_id" INTEGER NOT NULL,
+    "class_room_id" INTEGER NOT NULL,
+
+    CONSTRAINT "LectureClassRoom_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ClassRoom" ADD CONSTRAINT "ClassRoom_building_id_fkey" FOREIGN KEY ("building_id") REFERENCES "Building"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LectureClassRoom" ADD CONSTRAINT "LectureClassRoom_lecture_id_fkey" FOREIGN KEY ("lecture_id") REFERENCES "Lecture"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LectureClassRoom" ADD CONSTRAINT "LectureClassRoom_class_room_id_fkey" FOREIGN KEY ("class_room_id") REFERENCES "ClassRoom"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
