8e18f2075e0f5d8fbb30d5b0dd0c01880b3f2ff0
yuuki <bun.yuu88@gmail.com>
2025-03-07 18:22:08 +0900

    validateInputを導入し(丸パクリ)、エラーを文字列で返すようになりました

--- nestJS/src/listing-item/interface/listing-item.payload.ts
+++ nestJS/src/listing-item/interface/listing-item.payload.ts
@@ -1,49 +1,40 @@
+import { plainToInstance } from 'class-transformer';
 import {
   ArrayNotEmpty,
   ArrayUnique,
   IsArray,
   IsDate,
   IsFQDN,
+  IsIn,
   IsInt,
   IsOptional,
   IsString,
   Min,
+  validate
 } from 'class-validator';
 
-export class RecievedListingItemPayload {
-  @IsString()
-  documentId: string; //FirebaseにおけるID
-  @IsOptional()
-  @IsDate()
-  purchasedAt?: Date; //購入した日時
-  @IsOptional()
-  @IsString()
-  purchasedUserId?: string; //購入したユーザーのid
-  condition: Condition; //商品の状態
-  department: Academics; //学部
-  @IsOptional()
-  @IsString()
-  description?: string; //商品の説明
-  @IsArray()
-  @ArrayNotEmpty()
-  @ArrayUnique()
-  @IsString({each:true})
-  imageUrls:string[];
-  @IsInt()
-  @Min(0)
-  price: number; //商品の価格
-  @IsString()
-  name: string; //商品
-  @IsString()
-  firebaseUserId: string; //ユーザーID
+export async function validateInput<T extends object>(
+  cls: new () => T,
+  plain: object
+): Promise<T> {
+  const instance = plainToInstance(cls, plain);
+
+  const errors = await validate(instance);
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return instance;
 }
 
-export class ListingItemPayload extends RecievedListingItemPayload {
-  @IsInt()
-  id: Number; //ID
-  @IsDate()
-  createdAt: Date; //出品された日時
-}
+const allConditions = [
+'BRAND_NEW',
+'LIKE_NEW',
+'GOOD',
+'FAIR',
+'POOR',
+'BAD'];
+
 export type Condition =
   | 'BRAND_NEW' //新品、未使用
   | 'LIKE_NEW' //未使用に近い
@@ -51,6 +42,26 @@ export type Condition =
   | 'FAIR' //やや傷や汚れあり
   | 'POOR' //傷や汚れあり
   | 'BAD'; //全体的に状態が悪い
+
+const allAcademics = [
+  'All'
+  ,'LiberalArts'
+  ,'Law'
+  ,'SocialSciences'
+  ,'InternationalRelations'
+  ,'Literature'
+  ,'Business'
+  ,'PolicyScience'
+  ,'Psychology'
+  ,'GlobalLiberalArts'
+  ,'Film'
+  ,'InformationScience'
+  ,'ScienceAndTechnology'
+  ,'Economics'
+  ,'SportsHealthScience'
+  ,'FoodManagement'
+  ,'LifeSciences'
+  ,'Pharmacy'];
 
 export type Academics =
   | 'All'
@@ -71,3 +82,40 @@ export type Academics =
   | 'FoodManagement'
   | 'LifeSciences'
   | 'Pharmacy';
+
+export class RecievedListingItemPayload {
+  @IsString()
+  documentId: string; //FirebaseにおけるID
+  @IsOptional()
+  @IsDate()
+  purchasedAt?: Date; //購入した日時
+  @IsOptional()
+  @IsString()
+  purchasedUserId?: string; //購入したユーザーのid
+  @IsIn(allConditions)
+  condition: Condition; //商品の状態
+  @IsIn(allAcademics)
+  department: Academics; //学部
+  @IsOptional()
+  @IsString()
+  description?: string; //商品の説明
+  @IsArray()
+  @ArrayNotEmpty()
+  @ArrayUnique()
+  @IsString({each:true})
+  imageUrls:string[];
+  @IsInt()
+  @Min(0)
+  price: number; //商品の価格
+  @IsString()
+  name: string; //商品
+  @IsString()
+  firebaseUserId: string; //ユーザーID
+}
+
+export class ListingItemPayload extends RecievedListingItemPayload {
+  @IsInt()
+  id: number; //ID
+  @IsDate()
+  createdAt: Date; //出品された日時
+}
