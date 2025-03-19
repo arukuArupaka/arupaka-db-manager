286b4687da85952068fe1ac152b10e3c3f5c3c37
yuuki <bun.yuu88@gmail.com>
2025-03-20 02:16:31 +0900

    upsertをcreateに変更、それにあわせクラス名も変更。

--- nestJS/src/device-token/device-token.controller.ts
+++ nestJS/src/device-token/device-token.controller.ts
@@ -1,25 +1,29 @@
 import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
-import { DeviceTokenPayload} from './interface/device-token.payload';
+import { DeviceTokenPayload } from './interface/device-token.payload';
 import { DeviceTokenService } from './device-token.service';
 import { DeviceTokenDeleteInput } from './interface/device-token-delete.input';
-import { DeviceTokenUpsertInput } from './interface/device-token-upsert.input';
+import { DeviceTokenCreateInput } from './interface/device-token-create.input';
 
 @Controller('device_token')
 export class DeviceTokenController {
-    constructor(private deviceTokenService:DeviceTokenService){}
+  constructor(private deviceTokenService: DeviceTokenService) {}
 
-    @Get('get_all')
-    async getAllDeviceToken():Promise<DeviceTokenPayload[]>{
-        return await this.deviceTokenService.getAllDeviceToken();
-    }
+  @Get('get_all')
+  async getAllDeviceToken(): Promise<DeviceTokenPayload[]> {
+    return await this.deviceTokenService.getAllDeviceToken();
+  }
 
-    @Post('create')
-    async createDeviceToken(@Body() upsertDeviceToken:DeviceTokenUpsertInput,):Promise<string>{
-        return await this.deviceTokenService.createDeviceToken(upsertDeviceToken);
-    }
+  @Post('create')
+  async createDeviceToken(
+    @Body() upsertDeviceToken: DeviceTokenCreateInput,
+  ): Promise<string> {
+    return await this.deviceTokenService.createDeviceToken(upsertDeviceToken);
+  }
 
-    @Delete('delete')
-    async deleteDeviceToken(@Query() query:DeviceTokenDeleteInput):Promise<string>{
-        return await this.deviceTokenService.deleteDeviceToken(query);
-    }
+  @Delete('delete')
+  async deleteDeviceToken(
+    @Query() query: DeviceTokenDeleteInput,
+  ): Promise<string> {
+    return await this.deviceTokenService.deleteDeviceToken(query);
+  }
 }
--- nestJS/src/device-token/device-token.service.ts
+++ nestJS/src/device-token/device-token.service.ts
@@ -1,50 +1,59 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { DeviceTokenPayload } from './interface/device-token.payload';
 import { CustomPrismaService } from 'src/prisma/prisma.service';
-import { DeviceTokenUpsertInput } from './interface/device-token-upsert.input';
+import { DeviceTokenCreateInput } from './interface/device-token-create.input';
 import { DeviceTokenDeleteInput } from './interface/device-token-delete.input';
 import { validateValue } from 'src/common/validate-value';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class DeviceTokenService {
-    constructor(private readonly prismaService:CustomPrismaService){}
+  constructor(private readonly prisma: CustomPrismaService) {}
 
-    async getAllDeviceToken():Promise<DeviceTokenPayload[]>{
-        const allDeviceTokens:DeviceTokenPayload[] = await this.prismaService.deviceToken.findMany();
-        const validatedDeviceTokens = await Promise.all(allDeviceTokens.map((deviceToken)=>{
-            return validateValue(DeviceTokenPayload,deviceToken);
-        }))
-        return validatedDeviceTokens;
-    }
+  async getAllDeviceToken(): Promise<DeviceTokenPayload[]> {
+    const allDeviceTokens: DeviceTokenPayload[] =
+      await this.prisma.deviceToken.findMany();
+    const validatedDeviceTokens = await Promise.all(
+      allDeviceTokens.map((deviceToken) => {
+        return validateValue(DeviceTokenPayload, deviceToken);
+      }),
+    );
+    return validatedDeviceTokens;
+  }
 
-    async createDeviceToken(upsertDeviceToken:DeviceTokenUpsertInput):Promise<string>{
-        const UpsertedObject = await this.prismaService.deviceToken.upsert({
-            where: {
-                deviceToken: upsertDeviceToken.deviceToken
-            },
-            create: upsertDeviceToken,
-            update: {},
-        })
-        console.log(UpsertedObject);
-        if (!UpsertedObject){
-            return "creation failed";
-        }
-        return "upsert completed";  
+  async createDeviceToken(
+    upsertDeviceToken: DeviceTokenCreateInput,
+  ): Promise<string> {
+    try {
+      this.prisma.deviceToken.create({
+        data: upsertDeviceToken,
+      });
+      return 'create completed';
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        throw new BadRequestException();
+      }
     }
+  }
 
-    async deleteDeviceToken(deleteDeviceToken:DeviceTokenDeleteInput):Promise<string>{
-        try{    
-            await this.prismaService.deviceToken.delete({
-                where:{
-                    deviceToken: deleteDeviceToken.deviceToken,
-                }
-            })
-            return "delete completed";
-        }catch(e){
-            if (e instanceof PrismaClientKnownRequestError){
-                throw new NotFoundException(e)
-            }
-        }
+  async deleteDeviceToken(
+    deleteDeviceToken: DeviceTokenDeleteInput,
+  ): Promise<string> {
+    try {
+      await this.prisma.deviceToken.delete({
+        where: {
+          deviceToken: deleteDeviceToken.deviceToken,
+        },
+      });
+      return 'delete completed';
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        throw new NotFoundException(e);
+      }
     }
+  }
 }
