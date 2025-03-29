e7b9d1512d4a673ec97cace106b7f96cc5b00c0e
show-ritz <show.0417@icloud.com>
2025-03-29 16:45:37 +0900

    firebase_admin_sdkの導入

---
+++ nestJS/src/common/guards/auth.module.ts
@@ -0,0 +1,12 @@
+// ./src/guard/auth.module.ts
+
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+
+@Module({
+  providers: [AuthService, AuthGuard],
+  imports: [],
+  exports: [AuthService, AuthGuard],
+})
+export class AuthModule {}
