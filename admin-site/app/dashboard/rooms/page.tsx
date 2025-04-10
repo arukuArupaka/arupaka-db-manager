2ca935c274f166abbbdf2996248f52c2fa72a900
yuuki <bun.yuu88@gmail.com>
2025-04-10 23:50:57 +0900

    教室のページをエンドポイントなしで作れるところまで作りました。

--- admin-site/app/dashboard/rooms/page.tsx
+++ admin-site/app/dashboard/rooms/page.tsx
@@ -1,16 +1,40 @@
-import { DataTable } from "@/components/dashboard/data-table"
-import { columns } from "@/components/dashboard/rooms/columns"
-import { rooms } from "@/components/dashboard/rooms/data"
+import { DataTable } from "@/components/dashboard/data-table";
+import { columns } from "@/components/dashboard/rooms/columns";
+import { ClassRoom } from "@/components/interface/ClassRoom";
+import { LOCAL_DATABASE_URL } from "@/env";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+export default async function RoomsPage() {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("token")?.value;
 
-export default function RoomsPage() {
-  return (
-    <div className="space-y-6">
-      <div>
-        <h1 className="text-3xl font-bold tracking-tight">Room List</h1>
-        <p className="text-muted-foreground">Manage and view all rooms in the system.</p>
+  if (!token) {
+    redirect("/");
+  }
+  //教室を取得するエンドポイントをfetch(まだないけど)
+  try {
+    const res = await fetch(`${LOCAL_DATABASE_URL}/`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch textbooks: ${res.status}`);
+    }
+
+    const rooms: ClassRoom[] = await res.json();
+    return (
+      <div className="space-y-6">
+        <div>
+          <h1 className="text-3xl font-bold tracking-tight">Room List</h1>
+          <p className="text-muted-foreground">Manage and view all rooms in the system.</p>
+        </div>
+        {/* fetchした教室をdataに入れる */}
+        <DataTable columns={columns} data={rooms} />
       </div>
-      <DataTable columns={columns} data={rooms} />
-    </div>
-  )
+    );
+  } catch (error) {
+    console.error("Error fetching textbooks:", error);
+  }
 }
-
