ccec5260d7c345611cf28783e97b7577e7bfd573
show-ritz <show.0417@icloud.com>
2025-04-14 05:46:26 +0900

    修正

--- nestJS/src/line-bot/scheduleInitializerServise.ts
+++ nestJS/src/line-bot/scheduleInitializerServise.ts
@@ -20,12 +20,35 @@ export class ScheduleInitializerService implements OnModuleInit {
   async onModuleInit() {
     const schedules: Schedule[] = await this.prisma.schedule.findMany();
     const groupId = this.env.GroupId;
+    const convertWeekday = (weekday: string): number => {
+      switch (weekday) {
+        case 'Sunday':
+          return 0;
+        case 'Monday':
+          return 1;
+        case 'Tuesday':
+          return 2;
+        case 'Wednesday':
+          return 3;
+        case 'Thursday':
+          return 4;
+        case 'Friday':
+          return 5;
+        case 'Saturday':
+          return 6;
+      }
+    };
     schedules.forEach((schedule) => {
       const minute =
         schedule.minute === undefined || schedule.minute === null
           ? '*'
           : schedule.minute;
-      const cronTime = `${minute} ${schedule.hour ?? '*'} * * ${schedule.weekday ?? '*'}`;
+
+      const weekday =
+        schedule.weekday === undefined || schedule.weekday === null
+          ? '*'
+          : convertWeekday(schedule.weekday);
+      const cronTime = `${minute} ${schedule.hour ?? '*'} * * ${weekday}`;
       const job = new CronJob(cronTime, async () => {
         await this.lineBotService.sendMessage({
           groupId,
