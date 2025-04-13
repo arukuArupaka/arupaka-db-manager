6d8814f173af4fdc83fec95fa19c4e58f6c2f02b
show-ritz <show.0417@icloud.com>
2025-04-13 15:54:42 +0900

    テーブル作成の修正

--- nestJS/prisma/migrations/20250413065423_create_schedule_table/migration.sql
+++ nestJS/prisma/migrations/20250413065423_create_schedule_table/migration.sql
@@ -2,7 +2,9 @@
 CREATE TABLE "Schedule" (
     "id" TEXT NOT NULL,
     "schedule_id" TEXT NOT NULL,
-    "execute_time" TIMESTAMP(3) NOT NULL,
+    "minute" INTEGER NOT NULL,
+    "hour" INTEGER NOT NULL,
+    "day" INTEGER NOT NULL,
     "message" TEXT NOT NULL,
     "description" TEXT,
 
