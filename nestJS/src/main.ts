6a24da80fd9843b504a3094b011c3869086c8a74
show-ritz <show.0417@icloud.com>
2025-04-14 02:50:56 +0900

    CORSの修正

--- nestJS/src/main.ts
+++ nestJS/src/main.ts
@@ -12,11 +12,7 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://example.com',
-      'https://anotherdomain.com',
-    ],
+    origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
