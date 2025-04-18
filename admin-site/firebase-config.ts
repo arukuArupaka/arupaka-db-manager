46c8ec34c97fc365deed50a0e68b090edc29865f
show-ritz <show.0417@icloud.com>
2025-04-19 03:55:44 +0900

    修正

--- admin-site/firebase-config.ts
+++ admin-site/firebase-config.ts
@@ -6,7 +6,6 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
   apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
