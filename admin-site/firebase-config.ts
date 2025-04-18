81e7a54b7f07f37280681b5b1747468155ae8ac8
show-ritz <show.0417@icloud.com>
2025-04-19 03:59:04 +0900

    修正

--- admin-site/firebase-config.ts
+++ admin-site/firebase-config.ts
@@ -5,8 +5,6 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
   authDomain: `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}`,
