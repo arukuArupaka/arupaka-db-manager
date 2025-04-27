ba6a56c036346748ac13180c1cb590a177b7e6ca
yuuki <bun.yuu88@gmail.com>
2025-04-28 03:18:26 +0900

    cloudinary上でのid(publicId)を追加

--- nestJS/prisma/migrations/20250427172829_create_carousel_table/migration.sql
+++ nestJS/prisma/migrations/20250427172829_create_carousel_table/migration.sql
@@ -4,6 +4,10 @@ CREATE TABLE "Carousel" (
     "image_url" TEXT NOT NULL,
     "title" TEXT,
     "description" TEXT,
+    "public_id" TEXT NOT NULL,
 
     CONSTRAINT "Carousel_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Carousel_public_id_key" ON "Carousel"("public_id");
